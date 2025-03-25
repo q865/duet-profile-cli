75276c538e970f9b317929806a432579e0da44e8
q865 <men865@gmail.com>
2025-03-26 00:19:57 +0300

    fix

--- src/profile-generator.js
+++ src/profile-generator.js
@@ -1,3 +1,4 @@
+// src/profile-generator.js
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -12,21 +13,22 @@ export async function generateProfile(data) {
   try {
     await fs.mkdir(profileDir, { recursive: true });
 
-    // 1. Генерация README.md из шаблона
+    // Генерация README
     const template = await fs.readFile(
       path.join(__dirname, "../templates/readme-template.ejs"),
       "utf-8",
     );
-    const readmeContent = ejs.render(template, data);
 
-    // 2. Сохранение JSON-профиля
+    await fs.writeFile(
+      path.join(profileDir, "README.md"),
+      ejs.render(template, data),
+    );
+
+    // Сохранение JSON
     await fs.writeFile(
       path.join(profileDir, "profile.json"),
       JSON.stringify(data, null, 2),
     );
-
-    // 3. Сохранение README.md
-    await fs.writeFile(path.join(profileDir, "README.md"), readmeContent);
   } catch (err) {
     console.error(chalk.red("Ошибка генерации:"), err);
     throw err;
